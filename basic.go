cdf7d30cd06dd59cf8b8c22df9b629593152883b
Jony4 <jony4@yet-inc.com>
2019-04-20 23:55:11 +0800

    init qiye wechat

--- basic.go
+++ basic.go
@@ -3,4 +3,6 @@ package wechat
 const (
 	// MiniProgramBaseURI base uri
 	MiniProgramBaseURI = "api.weixin.qq.com"
+	// WorkBaseURI work base uri
+	WorkBaseURI = "qyapi.weixin.qq.com"
 )
---
+++ work_accesstoken.go
@@ -0,0 +1,121 @@
+package wechat
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+const (
+	// WorkAccessTokenEndpoint Endpoint
+	WorkAccessTokenEndpoint = "cgi-bin/gettoken"
+)
+
+// WorkAccessToken WorkAccessToken
+type WorkAccessToken struct {
+	client *Client
+
+	corpid     string
+	corpsecret string
+	grantType  string
+}
+
+// NewWorkAccessToken return instance of WorkAccessToken
+func NewWorkAccessToken(client *Client) *WorkAccessToken {
+	wat := &WorkAccessToken{
+		client: client,
+	}
+	wat.SetGrantType()
+	return wat
+}
+
+// SetSecret SetSecret
+func (wat *WorkAccessToken) SetSecret(corpsecret string) *WorkAccessToken {
+	wat.corpsecret = corpsecret
+	return wat
+}
+
+// SetAppID SetAppID
+func (wat *WorkAccessToken) SetAppID(corpid string) *WorkAccessToken {
+	wat.corpid = corpid
+	return wat
+}
+
+// SetGrantType SetGrantType
+func (wat *WorkAccessToken) SetGrantType() *WorkAccessToken {
+	wat.grantType = "client_credential"
+	return wat
+}
+
+// Validate checks if the operation is valid.
+func (wat *WorkAccessToken) Validate() error {
+	var invalid []string
+	if wat.corpid == "" {
+		invalid = append(invalid, "corpid")
+	}
+	if wat.corpsecret == "" {
+		invalid = append(invalid, "corpsecret")
+	}
+	if len(invalid) > 0 {
+		return fmt.Errorf("missing required fields: %v", invalid)
+	}
+	return nil
+}
+
+// Do Do
+func (wat *WorkAccessToken) Do(ctx context.Context) (*WorkAccessTokenResponse, error) {
+	// Check pre-conditions
+	if err := wat.Validate(); err != nil {
+		return nil, err
+	}
+	// url params
+	params := url.Values{}
+	params.Set("corpid", wat.corpid)
+	params.Set("corpsecret", wat.corpsecret)
+	params.Set("grant_type", wat.grantType)
+	// PerformRequest
+	res, err := wat.client.PerformRequest(ctx, PerformRequestOptions{
+		Method:   http.MethodGet,
+		Params:   params,
+		BaseURI:  WorkBaseURI,
+		Endpoint: WorkAccessTokenEndpoint,
+	})
+	if err != nil {
+		return nil, err
+	}
+	// Return operation response
+	ret := new(WorkAccessTokenResponse)
+	if err := wat.client.decoder.Decode(res.Body, ret); err != nil {
+		return nil, err
+	}
+	return ret, nil
+}
+
+// Credentials Credentials
+func (wat *WorkAccessToken) Credentials(ctx context.Context) (*AccessToken, error) {
+	res, err := wat.Do(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if res.ErrCode != 0 {
+		return nil, fmt.Errorf("errcode: %v, errmsg: %s", res.ErrCode, res.ErrMsg)
+	}
+	at := &AccessToken{
+		AccessToken: res.AccessToken,
+		ExpiresIn:   res.ExpiresIn,
+	}
+	return at, nil
+}
+
+// ToString ToString
+func (wat *WorkAccessToken) ToString() string {
+	return fmt.Sprintf("%s_%s_%s", wat.grantType, wat.corpid, wat.corpsecret)
+}
+
+// WorkAccessTokenResponse WorkAccessTokenResponse
+type WorkAccessTokenResponse struct {
+	CommonError
+	AccessToken string `json:"access_token"`
+	ExpiresIn   int64  `json:"expires_in"`
+}
