5e5a4f692bf51f2b9f2f1c0c73b4be176703017d
牛强 <jony4xx@gmail.com>
2021-01-04 00:31:27 +0800

    增加公众号发送客服消息的方法

--- officeaccount_msg_custom.go
+++ officeaccount_msg_custom.go
@@ -6,7 +6,7 @@ import (
 )
 
 const (
-	// OACustomMessageEndpoint https://developers.weixin.qq.com/miniprogram/dev/api-backend/open-api/subscribe-message/subscribeMessage.send.html
+	// OACustomMessageEndpoint https://developers.weixin.qq.com/doc/offiaccount/Message_Management/Service_Center_messages.html
 	OACustomMessageEndpoint = "cgi-bin/message/custom/send"
 )
 
@@ -18,61 +18,83 @@ type OACustomMessage struct {
 
 // OACustomMessageBody 消息体
 type OACustomMessageBody struct {
-	Touser          string `json:"touser"`
-	Msgtype         string `json:"msgtype"`
-	Miniprogrampage struct {
-		Title        string `json:"title"`
-		Appid        string `json:"appid"`
-		Pagepath     string `json:"pagepath"`
-		ThumbMediaID string `json:"thumb_media_id"`
-	} `json:"miniprogrampage"`
-	Text struct {
+	Touser          string                   `json:"touser"`
+	Msgtype         string                   `json:"msgtype"`
+	Miniprogrampage *OACustomMiniprogrampage `json:"miniprogrampage,omitempty"`
+	Text            *OACustomText            `json:"text,omitempty"`
+	Customservice   *OACustomservice         `json:"customservice,omitempty"`
+	Wxcard          *OACustomWXCard          `json:"wxcard,omitempty"`
+	Msgmenu         *OACustomMsgmenu         `json:"msgmenu,omitempty"`
+	Mpnews          *OACustomMpnews          `json:"mpnews,omitempty"`
+	News            *OACustomNews            `json:"news,omitempty"`
+	Music           *OACustomMusic           `json:"music,omitempty"`
+	Voice           *OACustomVoice           `json:"voice,omitempty"`
+	Video           *OACustomVideo           `json:"video,omitempty"`
+	Image           *OACustomImage           `json:"image,omitempty"`
+}
+
+type OACustomImage struct {
+	MediaID string `json:"media_id"`
+}
+
+type OACustomVideo struct {
+	MediaID      string `json:"media_id"`
+	ThumbMediaID string `json:"thumb_media_id"`
+	Title        string `json:"title"`
+	Description  string `json:"description"`
+}
+
+type OACustomVoice struct {
+	MediaID string `json:"media_id"`
+}
+
+type OACustomMusic struct {
+	Title        string `json:"title"`
+	Description  string `json:"description"`
+	Musicurl     string `json:"musicurl"`
+	Hqmusicurl   string `json:"hqmusicurl"`
+	ThumbMediaID string `json:"thumb_media_id"`
+}
+
+type OACustomNews struct {
+	Articles []struct {
+		Title       string `json:"title"`
+		Description string `json:"description"`
+		URL         string `json:"url"`
+		Picurl      string `json:"picurl"`
+	} `json:"articles,omitempty"`
+}
+
+type OACustomMpnews struct {
+	MediaID string `json:"media_id"`
+}
+
+type OACustomMsgmenu struct {
+	HeadContent string `json:"head_content"`
+	List        []struct {
+		ID      string `json:"id"`
 		Content string `json:"content"`
-	} `json:"text"`
-	Customservice struct {
-		KfAccount string `json:"kf_account"`
-	} `json:"customservice"`
-	Wxcard struct {
-		CardID string `json:"card_id"`
-	} `json:"wxcard"`
-	Msgmenu struct {
-		HeadContent string `json:"head_content"`
-		List        []struct {
-			ID      string `json:"id"`
-			Content string `json:"content"`
-		} `json:"list"`
-		TailContent string `json:"tail_content"`
-	} `json:"msgmenu"`
-	Mpnews struct {
-		MediaID string `json:"media_id"`
-	} `json:"mpnews"`
-	News struct {
-		Articles []struct {
-			Title       string `json:"title"`
-			Description string `json:"description"`
-			URL         string `json:"url"`
-			Picurl      string `json:"picurl"`
-		} `json:"articles"`
-	} `json:"news"`
-	Music struct {
-		Title        string `json:"title"`
-		Description  string `json:"description"`
-		Musicurl     string `json:"musicurl"`
-		Hqmusicurl   string `json:"hqmusicurl"`
-		ThumbMediaID string `json:"thumb_media_id"`
-	} `json:"music"`
-	Voice struct {
-		MediaID string `json:"media_id"`
-	} `json:"voice"`
-	Video struct {
-		MediaID      string `json:"media_id"`
-		ThumbMediaID string `json:"thumb_media_id"`
-		Title        string `json:"title"`
-		Description  string `json:"description"`
-	} `json:"video"`
-	Image struct {
-		MediaID string `json:"media_id"`
-	} `json:"image"`
+	} `json:"list,omitempty"`
+	TailContent string `json:"tail_content"`
+}
+
+type OACustomWXCard struct {
+	CardID string `json:"card_id"`
+}
+
+type OACustomservice struct {
+	KfAccount string `json:"kf_account"`
+}
+
+type OACustomText struct {
+	Content string `json:"content"`
+}
+
+type OACustomMiniprogrampage struct {
+	Title        string `json:"title"`
+	Appid        string `json:"appid"`
+	Pagepath     string `json:"pagepath"`
+	ThumbMediaID string `json:"thumb_media_id"`
 }
 
 // NewOACustomMessage 订阅消息
